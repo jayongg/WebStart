7b0d787ac251e22fbc65e005e1afcfae607a3995
jayongg <jayongg@microsoft.com>
2017-05-08 10:31:00 -0700

    add function

--- src/TestInitial.ts
+++ src/TestInitial.ts
@@ -1,7 +1,6 @@
 // Should we be referencing node.d.ts?
 // declare function require(name: string):any;f
 export module Localization {
-
 	export enum FontFamily {
 		Regular,
 		Bold,
@@ -10,4 +9,24 @@ export module Localization {
 		Semilight
 	}
 
+	export function getLanguageFileWithFallback(lang: string): string {
+		if (!lang) {
+			throw new Error("stringId must be a non-empty string, but was: " + lang);
+		}
+
+        var head = document.getElementsByTagName('head')[0];
+        var script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.onload = function () {
+            console.log("yeah!");
+        };
+        script.onerror = function () {
+            if (lang != "en-us") {
+                // try English
+                getLanguageFileWithFallback("en-us");
+            }
+        }
+        script.src = lang + "/onenote_strings.js";
+        head.appendChild(script);        
+	}
 }
---
+++ src/zh-cn/onenote_strings.js
@@ -0,0 +1,9 @@
+var JSADDINS = {};
+
+JSADDINS.Strings = function JSADDINS_Strings() {
+}
+
+
+ 
+JSADDINS.Strings.MD_NoInternet = 'You need to connect to the Internet to get meeting details';
+JSADDINS.Strings.MD_Retry = 'Retry';
